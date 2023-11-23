65fa92631010267cd3b531b192fc113fcfa8a4d3
Md Shawon Reza <fahadshawon241@gmail.com>
2023-11-24 04:14:58 +0600

    add this meals categories tabs  by show clicking categories meals

--- src/components/MealCard.jsx
+++ src/components/MealCard.jsx
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 const MealCard = ({item}) => {
     const {image,mealTitle,rating,price}=item
     return (
-        <div className="card w-96 bg-base-100 shadow-xl">
+        <div className="card  bg-base-100 shadow-xl">
             <figure><img className='w-full h-56' src={image} alt="Shoes" /></figure>
             <div className="card-body ">
                 <h2 className="card-title">{mealTitle}</h2>
